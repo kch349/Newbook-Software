2e6357da31651a5fe052eaef0a004d2e64548434
NDTH_Tech <ndthtech@ovid03.u.washington.edu>
2015-05-05 11:52:03 -0700

    got diary linker working

--- linker/arabicdiarylinker.sh
+++ linker/arabicdiarylinker.sh
@@ -7,7 +7,7 @@
 if [ "2" -eq $# ]
 then
     # AN_DD_PPP_LLL:XXX
-    sed -i.bak 's/\(AN_[0-9]\{2\}_[0-9]\{3\}_[0-9]\{3\}\)\(:[0-9]\{3\}\)/<a href=\"$2:\2\">\1\2<\/a>/g' $1
+    sed -i.bak "s@\(AN_[0-9]\{2\}_[0-9]\{3\}_[0-9]\{3\}\)\:([0-9]\{3\}\)@<a href=\"$2:\2\">\1:\2<\/a>@g" $1
 else
     echo 'Usage: arabicdiarylinker.sh <input_file> <table_url>'
 fi
--- linker/arabictablelinker.sh
+++ linker/arabictablelinker.sh
@@ -7,7 +7,7 @@
 if [ "1" -eq $# ]
 then
     # AN_DD_PPP_LLL:XXX
-    sed -i 's/\(AN_[0-9]\{2\}_[0-9]\{3\}_[0-9]\{3\}\)\(:[0-9]\{3\}\)/<a name=\"\2\">\1\2<\/a>/g' $1
+    sed -i 's/\(AN_[0-9]\{2\}_[0-9]\{3\}_[0-9]\{3\}\)\:([0-9]\{3\}\)/<a name=\"\2\">\1:\2<\/a>/g' $1
 else
     echo 'Usage: arabiclinkgen_table.sh <input_file>'
 fi
