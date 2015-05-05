0f84eeeec3fd61dc12d8eb90f845929e93dc49ee
NDTH_Tech <ndthtech@ovid03.u.washington.edu>
2015-05-05 11:41:59 -0700

    changes tablelinker to one argument, wrote diarylinker (untested)

---
+++ linker/arabicdiarylinker.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Adds links from diary's arabic notes to appropriate place in arabic notes table
+# Invoke using two arguments: string input_file, string table_url
+# Example: ./arabicdiarylinker.sh Diary_47_20150428.html http://depts.washington.edu/newbook/Diary_47_Arabic.html
+
+if [ "2" -eq $# ]
+then
+    # AN_DD_PPP_LLL:XXX
+    sed -i.bak 's/\(AN_[0-9]\{2\}_[0-9]\{3\}_[0-9]\{3\}\)\(:[0-9]\{3\}\)/<a href=\"$2:\2\">\1\2<\/a>/g' $1
+else
+    echo 'Usage: arabicdiarylinker.sh <input_file> <table_url>'
+fi
--- linker/arabictablelinker.sh
+++ linker/arabictablelinker.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Adds anchors to HTML file of Arabic notes table
-# Invoke using two arguments: string input_file, string output_file
-# Example: ./arabiclinkgen_table.sh Diary_47_Arabic.html Diary_47_Arabic_Anchors.html
+# Invoke using one argument: string input_file
+# Example: ./arabictablelinker.sh Diary_47_Arabic.html
 
-if [ "2" -eq $# ]
+if [ "1" -eq $# ]
 then
     # AN_DD_PPP_LLL:XXX
-    sed -i 's/\(AN_[0-9]\{2\}_[0-9]\{3\}_[0-9]\{3\}\)\(:[0-9]\{3\}\)/<a name=\"\2\">\1\2<\/a>/g' $1 > $2
+    sed -i 's/\(AN_[0-9]\{2\}_[0-9]\{3\}_[0-9]\{3\}\)\(:[0-9]\{3\}\)/<a name=\"\2\">\1\2<\/a>/g' $1
 else
-    echo 'Usage: arabiclinkgen_table.sh <input_file> <output_file>'
+    echo 'Usage: arabiclinkgen_table.sh <input_file>'
 fi
