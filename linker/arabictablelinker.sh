8f5f533c8bd9d40b309239921b2f214430cbf765
NDTH_Tech <ndthtech@ovid03.u.washington.edu>
2015-05-13 10:31:21 -0700

    fixed more formatting issues

--- linker/arabictablelinker.sh
+++ linker/arabictablelinker.sh
@@ -6,8 +6,8 @@
 
 if [ "1" -eq $# ]
 then
-    # A_DD_PPP_LLL:XXX
-    sed -i.bak 's/\(A_[0-9]\{2\}_[0-9]\{3\}_[0-9]\{3\}\)\:([0-9]\{3\}\)/<a name=\"\2\">\1:\2<\/a>/g' $1
+    # ADD_PPP_LL:XXX
+    sed -i.bak 's/\(A[0-9]\{2\}_[0-9]\{3\}_[0-9]\{2\}\)\:([0-9]\{3\}\)/<a name=\"\2\">\1:\2<\/a>/g' $1
 else
     echo 'Usage: arabiclinkgen_table.sh <input_file>'
 fi
